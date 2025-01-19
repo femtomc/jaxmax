dc37755afd943e7bfc283fadeaa10305ea003709
femtomc <mccoybecker@gmail.com>
2025-01-18 21:53:34 -0500

    Clean.

--- examples/transformations.py
+++ examples/transformations.py
@@ -1,11 +1,14 @@
 from juju import jit
-from jax import grad, vmap
+from jax import grad, vmap, make_jaxpr
 import jax.numpy as jnp
 
 
 def f(x):
     return x**2
 
+
+fn = grad(lambda x: jnp.sum(vmap(f)(x)))
+print(make_jaxpr(fn)(jnp.array([1.0, 2.0, 3.0, 4.0, 5.0])))
 
 loss = jit(grad(lambda x: jnp.sum(vmap(f)(x))))
 
