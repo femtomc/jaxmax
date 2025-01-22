79f918c36c39d7346f65d3cc375451d83fc58413
femtomc <mccoybecker@gmail.com>
2025-01-22 14:05:19 -0500

    Add session keyword to JITEngine.

--- src/juju/compiler.py
+++ src/juju/compiler.py
@@ -265,7 +265,7 @@ def pjit(*args, **params):
 @dataclass
 class JITEngine:
     cache: dict = field(default_factory=dict)
-    session = engine.InferenceSession(
+    session: engine.InferenceSession = engine.InferenceSession(
         custom_extensions="./kernels.mojopkg",
         devices=[CPU()],
     )
@@ -292,7 +292,7 @@ def gpu_engine():
         session=engine.InferenceSession(
             custom_extensions="./kernels.mojopkg",
             devices=[Accelerator()],
-        )
+        ),
     )
 
 
